1ddea505eaba4605e97dff633703cd2427e1730d
Zhao Peng <123764772@qq.com>
2013-03-20 16:39:00 +0800

    后台

--- coolpipi/src/com/cool/dao/impl/ProductDAOImpl.java
+++ coolpipi/src/com/cool/dao/impl/ProductDAOImpl.java
@@ -3,23 +3,41 @@ package com.cool.dao.impl;
 import com.cool.dao.ProductDAO;
 import com.cool.model.DetailProduct;
 import com.cool.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * @author zorro
  * Product数据库层实现类
  */
-public class ProductDAOImpl implements ProductDAO {
+public class ProductDAOImpl extends JdbcDaoSupport implements ProductDAO {
+    private static final String SELECT_PRODUCT_INDEX = "select uid,name,summary,defaultpic from c_product order by pv desc limit 8";
+    private static final String SELECT_PRODUCT_PAGE = "select uid,name,summary,defaultpic from c_product order by pv desc limit ?,8";
+
     public List<Product> getProduct() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return getJdbcTemplate().query(SELECT_PRODUCT_INDEX, new ProductRowMapper());
     }
 
     public List<Product> getProductByPage(int pageNum) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return getJdbcTemplate().query(SELECT_PRODUCT_PAGE, new Object[]{pageNum + 1} ,new ProductRowMapper());
     }
 
     public DetailProduct getDetailProduct(int uid) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private class ProductRowMapper implements RowMapper {
+        public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Product product = new Product();
+            product.setUid(rs.getInt("uid"));
+            product.setName(rs.getString("name"));
+            product.setSummary(rs.getString("summary"));
+            product.setDefaultPic(rs.getString("defaultpic"));
+            return product;
+        }
     }
 }
---
+++ coolpipi/src/com/cool/web/IndexController.java
@@ -0,0 +1,29 @@
+package com.cool.web;
+
+import com.cool.dao.ProductDAO;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zorro
+ * 首页内容的控制器
+ */
+public class IndexController extends AbstractController {
+    private ProductDAO productDAO;
+
+    public void setProductDAO(ProductDAO productDAO){
+        this.productDAO = productDAO;
+    }
+
+    protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        Map model = new HashMap();
+        model.put("products", productDAO.getProduct());
+        return new ModelAndView("index", model);
+    }
+}
